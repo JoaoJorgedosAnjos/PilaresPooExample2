772d71a0cad00a869f9700506f98b41aaffa0e21
joaojorge <joaojorgedosanjos@gmail.com>
2023-06-25 10:22:34 -0300

    Aula herança, entendendo classe pai e herança na classe filha

--- src/ComputadorPedrinho.java
+++ src/ComputadorPedrinho.java
@@ -1,10 +1,19 @@
 public class ComputadorPedrinho {
     public static void main(String[] args) {
         //abrindo MSN Messenger
+        System.out.println("MSN");
         MSNMessenger msn = new MSNMessenger();
-
         msn.enviarMensagem();
-
         msn.receberMensagem();
+
+        System.out.println("FACEBOOK");
+        FacebookMessenger fbm = new FacebookMessenger();
+        fbm.enviarMensagem();
+        fbm.enviarMensagem();
+
+        System.out.println("TELEGRAM");
+        Telegram tlg = new Telegram();
+        tlg.enviarMensagem();
+        tlg.enviarMensagem();
     }
 }
