fd943270c5c8a683cd608e4f224ce5d99c511fba
joaojorge <joaojorgedosanjos@gmail.com>
2023-06-25 10:10:26 -0300

    Added comentários

--- src/MSNMessenger.java
+++ src/MSNMessenger.java
@@ -1,12 +1,20 @@
 public class MSNMessenger {
     public void enviarMensagem() {
+        //primeiro confirmar se esta conectado a internet
         validarConectadoInternet();
+
         System.out.println("Enviando mensagem");
+
+        //depois de enviada, salva o histórico da mensagem
         salvarHistoricoMensagem();
+
+
     }
     public void receberMensagem() {
         System.out.println("Recebendo mensagem");
     }
+
+    //métodos privadas, visíveis somente na classe
     private void validarConectadoInternet() {
         System.out.println("Validando se está conectado a internet");
     }
